740fb35022c02e74a1421b5d81bf6e041bc05639
Greaziel <greaziel@gmail.com>
2025-06-04 23:25:52 -0300

    Criando o Projeto

---
+++ src/main/java/com/example/desafioluizalabspessoadesenvolvedorabackend/DesafioluizalabsApplication.java
@@ -0,0 +1,13 @@
+package com.example.desafioluizalabspessoadesenvolvedorabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioluizalabsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioluizalabsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/desafioluizalabspessoadesenvolvedorabackend/DesafioluizalabsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.desafioluizalabspessoadesenvolvedorabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioluizalabsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
