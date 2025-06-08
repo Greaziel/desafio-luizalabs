d46ccbd227b1c808271fd1e72eb4ca7656d8a68e
Greaziel <greaziel@gmail.com>
2025-06-08 15:52:10 -0300

    Ajustando o resto do controller

--- src/main/java/com/example/desafioluizalabspessoadesenvolvedorabackend/controller/CompraController.java
+++ src/main/java/com/example/desafioluizalabspessoadesenvolvedorabackend/controller/CompraController.java
@@ -1,14 +1,13 @@
 package com.example.desafioluizalabspessoadesenvolvedorabackend.controller;
 
-import com.example.desafioluizalabspessoadesenvolvedorabackend.dtos.CompraDTO;
+import com.example.desafioluizalabspessoadesenvolvedorabackend.dtos.FiltroDataDTO;
 import com.example.desafioluizalabspessoadesenvolvedorabackend.dtos.UsuarioDTO;
 import com.example.desafioluizalabspessoadesenvolvedorabackend.services.CompraService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
@@ -21,7 +20,29 @@ public class CompraController {
     private final CompraService compraService;
 
     @PostMapping(value = "/processar", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public List<UsuarioDTO> processarArquivo(@RequestParam("arquivo") MultipartFile file) throws Exception {
-        return compraService.consultarPedidos(file.getInputStream());
+    public ResponseEntity<List<UsuarioDTO>> processarArquivo(@RequestParam("arquivo") MultipartFile file) throws Exception {
+        return ResponseEntity.ok(compraService.processarArquivo(file.getInputStream()));
+    }
+
+    @GetMapping(value = "/consultar")
+    public ResponseEntity<List<UsuarioDTO>> consultarTodos(){
+        return ResponseEntity.ok(compraService.consultarUsuarios());
+    }
+
+    @GetMapping(value = "/consultar/{id}")
+    public ResponseEntity<List<UsuarioDTO>> consultarPorId(@PathVariable Integer id){
+        return ResponseEntity.ok(compraService.consultarPedidosPorId(id));
+    }
+
+    @PostMapping(value = "/consultardata")
+    public ResponseEntity<?> consultarTodos(@RequestBody FiltroDataDTO filtro){
+        List<UsuarioDTO> resultado = compraService.consultarPedidosPorData(filtro.getDataInicial(), filtro.getDataFinal());
+
+        if (resultado.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Não foram encontrados dados.");
+        }
+
+        return ResponseEntity.ok(resultado);
+
     }
 }
