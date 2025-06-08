ae7fbf25e92545118e5f594eb8119a9c57cfc174
Greaziel <greaziel@gmail.com>
2025-06-07 22:48:48 -0300

    Construindo as Clases de DTO

---
+++ src/main/java/com/example/desafioluizalabspessoadesenvolvedorabackend/dtos/ProdutoDTO.java
@@ -0,0 +1,10 @@
+package com.example.desafioluizalabspessoadesenvolvedorabackend.dtos;
+
+import lombok.Data;
+
+@Data
+public class ProdutoDTO {
+
+    private Integer produto_id;
+    private String value;
+}
